316c9e965877f4bd73ac8e27760e5cc6913f8ffe
Kevin Phelps <kphelps@engineyard.com>
2014-03-26 13:53:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ocfs2app::Application.config.secret_key_base = 'f3a84e5c0e0b5d99d76d731f3a5f8a80dbeb06892d01e39ecba413647af26b21388dc489b3d93eec39b131085aa1b1e587bca30684912e8e6d32c9eece8eb8d6'
